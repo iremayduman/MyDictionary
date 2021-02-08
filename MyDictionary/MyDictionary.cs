8ddb0aa6778f1556a77cda554ea17aad08c278ad
İrem <77546520+iremayduman@users.noreply.github.com>
2021-02-08 22:14:51 +0300

    MyDictionary
    Generik yapısı ile sözlük oluşturma

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T>
+    {
+        T[] array;
+        T[] tempArray;//geçiçi dizi
+
+        public MyDictionary()
+        {
+            array = new T[0];
+        }
+        public void Add(T item)
+        {
+            tempArray = array;//diziyi geçici diziye eşitledik çünkü yeni eleman eklendiğinde eski eleman silinecek.
+            array = new T[array.Length+1];
+            for (int i = 0; i < tempArray.Length; i++)//geçici dizi elemanları 1 arttırılır.
+            {
+                array[i] = tempArray[i];//normal listedeki eski elemanlar silinmesin diye geçici diziye aktarmıştık.Burada geçici diziye aktarılan elemanları geri aldık.
+                
+            }
+            array[array.Length - 1] = item;
+        }
+        public int Count
+        {
+            get { return array.Length; }
+        }
+        public void All()
+        {
+            foreach(T isim in array)
+            {
+                Console.WriteLine("İsim: " +isim);
+            }
+        }
+
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> isimler = new MyDictionary<string>();
+            isimler.Add("İrem");
+            isimler.Add("Azra");
+            isimler.Add("Can");
+            isimler.Add("Görkem");
+            isimler.Add("Gizem");
+            Console.WriteLine("Eleman sayısı: "+isimler.Count);
+            isimler.All();
+        }
+    }
+}
